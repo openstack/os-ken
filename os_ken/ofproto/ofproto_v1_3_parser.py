2f30f44406535991ec982608d04c8893b8fda9ad
elajkat <lajos.katona@est.tech>
2024-06-06 12:34:03 +0200

    Raise ValueError in case unpack_from returns zero length
    Closes-Bug: #2067973
    Closes-Bug: #2067970
    Change-Id: If3327be6c0a4c25173473fb8879d111544d77af5

--- os_ken/ofproto/ofproto_v1_3_parser.py
+++ os_ken/ofproto/ofproto_v1_3_parser.py
@@ -1616,6 +1616,8 @@ class OFPPropBase(StringifyMixin):
     @classmethod
     def parse(cls, buf):
         (type_, length) = struct.unpack_from(cls._PACK_STR, buf, 0)
+        if not length:
+            raise ValueError
         rest = buf[utils.round_up(length, 8):]
         try:
             subcls = cls._TYPES[type_]
@@ -6602,6 +6604,8 @@ class NXTPacketIn2Prop(OFPPropBase):
     @classmethod
     def parse(cls, buf):
         (type_, length) = struct.unpack_from(cls._PACK_STR, buf, 0)
+        if not length:
+            raise ValueError
         rest = buf[utils.round_up(length, 8):]
         try:
             subcls = cls._TYPES[type_]
